f4a16e923b4cb7ea635e3731050d3ce2bfc643c1
Aditya <aditya.shukla278@gmail.com>
2022-06-15 18:20:44 +0530

    minor change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send({message: 'Hello World!'});
   console.log(res.statusCode + ' ' + req.method + ' ' + req.url );
 });
 
